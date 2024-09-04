a1ed8a1d0baeeaee0d0cb306bf5e515e92dad86c
FrostyForest <748773880@qq.com>
2024-09-04 13:41:15 +0800

    add_sensor

--- test_code/manually_spawn_assets.py
+++ test_code/manually_spawn_assets.py
@@ -22,23 +22,27 @@ import omni.isaac.lab.sim as sim_utils
 
 from omni.isaac.lab.utils.assets import ISAAC_NUCLEUS_DIR
 def set_up_scene():
-    cfg_ground = sim_utils.GroundPlaneCfg()
-    cfg_ground.func("/World/defaultGroundPlane", cfg_ground)
+    # cfg_ground = sim_utils.GroundPlaneCfg()
+    # cfg_ground.func("/World/defaultGroundPlane", cfg_ground)
+    #
+    # cfg_cone_rigid = sim_utils.ConeCfg(
+    #     radius=0.15,
+    #     height=0.5,
+    #     rigid_props=sim_utils.RigidBodyPropertiesCfg(),
+    #     mass_props=sim_utils.MassPropertiesCfg(mass=1.0),
+    #     collision_props=sim_utils.CollisionPropertiesCfg(),
+    #     visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=(0.0, 1.0, 0.0)),
+    # )
+    # cfg_cone_rigid.func(
+    #     "/World/Objects/ConeRigid", cfg_cone_rigid, translation=(-0.2, 0.0, 2.0), orientation=(0.5, 0.0, 0.5, 0.0)
+    # )
+    #
+    # light_cfg = sim_utils.DomeLightCfg(intensity=2000.0, color=(0.75, 0.75, 0.75))
+    # light_cfg.func("/World/Light", light_cfg)
 
-    cfg_cone_rigid = sim_utils.ConeCfg(
-        radius=0.15,
-        height=0.5,
-        rigid_props=sim_utils.RigidBodyPropertiesCfg(),
-        mass_props=sim_utils.MassPropertiesCfg(mass=1.0),
-        collision_props=sim_utils.CollisionPropertiesCfg(),
-        visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=(0.0, 1.0, 0.0)),
-    )
-    cfg_cone_rigid.func(
-        "/World/Objects/ConeRigid", cfg_cone_rigid, translation=(-0.2, 0.0, 2.0), orientation=(0.5, 0.0, 0.5, 0.0)
-    )
-
-    light_cfg = sim_utils.DomeLightCfg(intensity=2000.0, color=(0.75, 0.75, 0.75))
-    light_cfg.func("/World/Light", light_cfg)
+    # spawn a usd file of a table into the scene
+    cfg = sim_utils.UsdFileCfg(usd_path=f"{ISAAC_NUCLEUS_DIR}/Environments/Simple_Room/simple_room.usd")
+    cfg.func("/World/hospital", cfg, translation=(0.0, 0.0, 0))
 
 
 def main():
